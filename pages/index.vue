139b0c6be6be1fbd9ee720562dde2ff74d8ea61e
AlexLee87 <alexlee9988@gmail.com>
2022-04-03 15:38:17 +0800

    Added new burger photo and text changed

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="header" class="bg-[url('/hero-bg.jpg')] pl-20 pr-20">
-    <div id='menu' class='grid grid-cols-2 pt-5 pb-5'>
-        <div id='menu-left'>
-          <img src='logo.png' class='w-[150px]' alt=''>
+    <div id="menu" class="grid grid-cols-2 pt-5 pb-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="">
         </div>
-        <div id='menu-right'>
-          <ul class='grid grid-cols-4 text-lg font-oswald
-          text-white uppercase pt-5'>
+        <div id="menu-right">
+          <ul class="grid grid-cols-4 text-lg font-oswald
+          text-white uppercase pt-5">
             <li>About Us</li>
             <li>Our Menu</li>
             <li>Locations</li>
             <li>Contact Us</li>
           </ul>
         </div>
+    </div>
+    <div class="grid grid-cols-2 pb-20 pt-10">
+      <img src="hero-burger.png" class="pr-8 mt-10" alt=""/>
+      <div>
+        <h1 class="text-8xl text-white text-center font-lilita uppercase my-5">Classic Burger</h1>
+        <h2 class="6xl text-white text-center font-lilita uppercase my-5">
+            With Onion
+        </h2>
+        <p class="text-white font-roboto font-light text-center">
+          Our signature burger now comes with onion and cheese.You can customize it to
+          your likings. Freshly made to order, all in a brioche style bun.
+        </p>
+      </div>
     </div>
   </div>
 </template>
