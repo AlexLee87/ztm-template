fc8ebc34a75775e5e538d5e0fa20146257c3098e
AlexLee87 <alexlee9988@gmail.com>
2022-04-03 12:43:33 +0800

    Updated logo and background added right sided menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Blocktopia</h1>
-      <p class="mt-5">Here you can learn about blockchain</p>
-    <div id="image">
-        <img src="blockchain002.jpg" alt="blockchainImage" width="600" height="600">
-    </div>
+  <div id="header" class="bg-[url('/hero-bg.jpg')] pl-20 pr-20">
+    <div id='menu' class='grid grid-cols-2 pt-5 pb-5'>
+        <div id='menu-left'>
+          <img src='logo.png' class='w-[150px]' alt=''>
+        </div>
+        <div id='menu-right'>
+          <ul class='grid grid-cols-4 text-lg font-oswald
+          text-white uppercase pt-5'>
+            <li>About Us</li>
+            <li>Our Menu</li>
+            <li>Locations</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
     </div>
   </div>
 </template>
